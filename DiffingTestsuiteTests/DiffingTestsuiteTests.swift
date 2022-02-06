ebfa9a0b291bc36b7e5754fb8cc5351cd6d347c2
Patrick Dinger <121539+paxos@users.noreply.github.com>
2022-02-06 10:55:13 -0800

    Add random tests

--- DiffingTestsuiteTests/DiffingTestsuiteTests.swift
+++ DiffingTestsuiteTests/DiffingTestsuiteTests.swift
@@ -42,14 +42,14 @@ func differ(start: [Int], end: [Int]) -> [Int] {
 
     for patch in patches {
         switch patch {
-        case .insertion(index: let index, element: let element):
-            print("INSERT", index)
+        case let .insertion(index: index, element: element):
+            print("INSERT \(element) at", index, workingSet)
             workingSet.insert(element, at: index)
         case let .deletion(index: index):
-            print("REMOVE", index)
+            print("REMOVE \(workingSet[index]) at", index, workingSet)
             workingSet.remove(at: index)
         case let .move(from: from, to: to):
-            print("MOVE", from, to)
+            print("MOVE number \(workingSet[from]) from", from, to, workingSet)
             let val = workingSet[from]
             workingSet.remove(at: from)
             workingSet.insert(val, at: to)
@@ -103,5 +103,36 @@ class DiffingTestsuiteTests: XCTestCase {
 
         let result = differ(start: start, end: target)
         XCTAssertEqual(result, target)
+    }
+
+    func testDifferRandom() throws {
+        let NUMBER_OF_TESTS = 10000
+        let MAX_SIZE = 25
+        let MAX_NUMBER = 10
+
+        var testPairs: [[[Int]]] = []
+
+        for _ in 1 ... NUMBER_OF_TESTS {
+            let b = [makeArray(maxSize: MAX_SIZE, maxNumber: MAX_NUMBER), makeArray(maxSize: MAX_SIZE, maxNumber: MAX_NUMBER)]
+            testPairs.append(b)
+        }
+
+        // Run tests
+        testPairs.forEach { testPair in
+            let input = testPair[0]
+            let output = testPair[1]
+
+            print("-------------------------------------")
+            print("Input: \(input), Output: \(output)")
+
+            let result = differ(start: input, end: output)
+            print("Result: \(result)")
+
+            XCTAssertEqual(output, result)
+        }
+    }
+
+    func makeArray(maxSize: Int, maxNumber: Int) -> [Int] {
+        return Array(repeating: 0, count: Int.random(in: 0 ... maxSize)).map { _ in Int.random(in: 0 ... maxNumber) }
     }
 }
