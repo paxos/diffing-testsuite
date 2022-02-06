1ee1fa6b362ce15a490ca98d5e9d0fb77bf46b3e
Patrick Dinger <121539+paxos@users.noreply.github.com>
2022-02-05 22:31:48 -0800

    Initial Commit

---
+++ DiffingTestsuite/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DiffingTestsuite
+//
+//  Created by Patrick Dinger on 2/5/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
