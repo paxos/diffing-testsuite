228e9b0773393e9d8d9653ef08320970bde7a0f2
Patrick Dinger <121539+paxos@users.noreply.github.com>
2022-02-06 18:17:17 -0800

    Add move test for DifferenceKit

--- DiffingTestsuiteTests/DifferenceKitTests.swift
+++ DiffingTestsuiteTests/DifferenceKitTests.swift
@@ -16,27 +16,62 @@ class DifferenceKitTests: XCTestCase {
         XCTAssertEqual(output, result)
     }
     
+    func testScenario10() throws {
+        let input = [1, 2, 3, 4]
+        let output = [1, 2, 3]
+        let result = runDifferenceKit(start: input, end: output)
+        XCTAssertEqual(output, result)
+    }
+
     func testScenario2() throws {
-        let input = [1,2,4]
-        let output = [1,2,3,4]
+        let input = [1, 2, 4]
+        let output = [1, 2, 3, 4]
+        let result = runDifferenceKit(start: input, end: output)
+        XCTAssertEqual(output, result)
+    }
+
+    func testScenario3() throws {
+        let input = [1, 2, 3, 4, 5]
+        let output = [5, 4, 3, 2, 1]
+        let result = runDifferenceKit(start: input, end: output)
+        XCTAssertEqual(output, result)
+    }
+
+    func testScenario4() throws {
+        let input = [1, 2, 3]
+        let output = [3, 2, 1]
+        let result = runDifferenceKit(start: input, end: output)
+        XCTAssertEqual(output, result)
+    }
+
+    func testScenario5() throws {
+        let input = [0, 1, 2, 3]
+        let output = [3, 2, 1, 0]
+        let result = runDifferenceKit(start: input, end: output)
+        XCTAssertEqual(output, result)
+    }
+    
+    func testScenario6() throws {
+        let input = ["zero", "one", "two", "Three"]
+        let output = ["three", "two", "one", "zero"]
+        let result = runDifferenceKit(start: input, end: output)
+        XCTAssertEqual(output, result)
+    }
+    
+    func testScenario7() throws {
+        let input = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        let output = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
         let result = runDifferenceKit(start: input, end: output)
         XCTAssertEqual(output, result)
     }
 }
 
 func runDifferenceKit<T: Differentiable>(start: [T], end: [T]) -> [T] {
-    let changeSet = StagedChangeset(source: start, target: end)
+    let changeSet = StagedChangeset(source: start, target: end, section: 0)
 
     var workingSet = start
 
     for patch in changeSet {
-        patch.elementInserted.forEach {
-            let index = $0.element
-            let element = end[index]
-            print("INSERT \(element) at", index)
-            workingSet.insert(element, at: index)
-            print(workingSet)
-        }
         patch.elementDeleted.forEach {
             let index = $0.element
             let element = start[index]
@@ -44,19 +79,39 @@ func runDifferenceKit<T: Differentiable>(start: [T], end: [T]) -> [T] {
             workingSet.remove(at: index)
             print(workingSet)
         }
-        patch.elementMoved.forEach {
-            let from = $0.source.element
-            let to = $0.target.element
+
+        patch.elementInserted.forEach {
+            let index = $0.element
+            let element = end[index]
+            print("INSERT \(element) at", index)
+            workingSet.insert(element, at: index)
+            print(workingSet)
+        }
+
+        patch.elementUpdated.forEach {
+            print("updated \($0)")
+        }
+
+        for (source, target) in patch.elementMoved {
+            let from = source.element
+            let to = target.element
             let element = start[from]
 
             print("MOVE number \(element) from", from, to, workingSet)
-            let val = element
-            workingSet.remove(at: from)
-            workingSet.insert(val, at: to)
+
+            // Find elementA in start at index from
+            // Find indexA of elementA in working set
+            // Swap indexA with to
+
+            let elementA = start[from]
+            let indexA = workingSet.firstIndex(where: {
+                $0.isContentEqual(to: elementA)
+            })!
+
+            workingSet.remove(at: indexA)
+            workingSet.insert(elementA, at: to)
+
             print(workingSet)
-        }
-        patch.elementUpdated.forEach {
-            print("updated \($0)")
         }
     }
 
@@ -64,3 +119,4 @@ func runDifferenceKit<T: Differentiable>(start: [T], end: [T]) -> [T] {
 }
 
 extension Int: Differentiable {}
+extension String: Differentiable {}
